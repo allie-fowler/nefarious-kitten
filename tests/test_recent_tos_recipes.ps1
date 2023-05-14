84e771898ff6663be28e2f0113cb6c04309a4917
Allie F <67930016+allie-fowler@users.noreply.github.com>
2023-05-13 20:09:22 -0400

    Update test_recent_tos_recipes.ps1

--- tests/test_recent_tos_recipes.ps1
+++ tests/test_recent_tos_recipes.ps1
@@ -16,7 +16,17 @@ Describe "Testing scripts/tos/recent_recipes_from_tos.ps1" {
             $output = & "scripts/tos/recent_recipes_from_tos.ps1"
 
             # Define the expected output
-            $expectedOutput = "Hello, World!"
+            $expectedOutput = "AAPL		 Recipe
+ACN		 Recipe
+CF		 Recipe
+EXAS		 Recipe
+FDX		 Recipe
+SNOW		 Recipe
+SPWR		 Moses
+SPWR		 Recipe
+TDOC		 Recipe
+TEAM		 Recipe
+URI		 Recipe"
             
             # Assert the test
             $output | Should Be $expectedOutput
