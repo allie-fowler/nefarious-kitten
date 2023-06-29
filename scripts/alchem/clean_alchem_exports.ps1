1c9072a24356ad7f8f3153f427aa118b8b0fd6c8
Allie F <67930016+allie-fowler@users.noreply.github.com>
2023-06-29 13:08:13 -0400

    Update clean_alchem_exports.ps1

--- scripts/alchem/clean_alchem_exports.ps1
+++ scripts/alchem/clean_alchem_exports.ps1
@@ -6,10 +6,24 @@ param (
 
 # Check if the input file parameter is empty
 if ([string]::IsNullOrEmpty($InputFile)) {
-    $inputFile = Get-ChildItem -Path . -Filter "input/*recipe*UP.sym" | Select-Object -First 1
+    $inputFile = Get-ChildItem -Path . -Filter "input/recipe*UP.sym" | Select-Object -First 1
 }
 
-$symbols = Get-Content $inputFile
+# Check if a matching file was found
+if ($inputFile) {
+    # Use the file as  input
+    # $inputFile.FullName contains the full path to the file
+    Write-Host "Using file: $($inputFile)"
+    
+    Write-Host "File Modify Date: $fileCreationDate"
+
+    $datenow = Get-Date -Format "MMddyyyy-HHmmss"
+    $DaysAgo = (Get-Date).AddDays($lookBack)
+    $regex = "\b[A-Z]{1,4}\b"
+
+    # Find dates in the specified range, grab stock symbols and recipe type
+    $symbols = Get-Content -Path $inputFile Sort-Object -Unique | Out-String
+} 
 
 $watchlist = @{
     name = "mywatchlist"
@@ -19,11 +33,13 @@ $watchlist = @{
         }
     )
 }
-$json = $watchlist | ConvertTo-Json
-$headers = @{
-    Authorization = "Bearer <access_token>"
-    ContentType = "application/json"
-}
+
+Write-Host "UP recipes: $watchlist"
+#$json = $watchlist | ConvertTo-Json
+#$headers = @{
+#    Authorization = "Bearer <access_token>"
+#    ContentType = "application/json"
+#}
 
 # Later we might add directly to ToS with the following technique
 #Invoke-RestMethod -Uri "https://api.tdameritrade.com/v1/accounts/<account_id>/watchlists/<watchlist_id>" -Method Put -Headers $headers -Body $json
