60c3b4333d038b608a6dad098c8abb9b05831a14
Allie F <67930016+allie-fowler@users.noreply.github.com>
2023-11-25 10:24:22 -0500

    Update recent_recipes_from_tos.ps1

--- scripts/tos/recent_recipes_from_tos.ps1
+++ scripts/tos/recent_recipes_from_tos.ps1
@@ -91,7 +91,7 @@ if ($inputFile) {
     
     $sortedSymbolsList = $symbolsHashtable.GetEnumerator() | Sort-Object Name | ForEach-Object {
         $stockRecipe = $_.Name -split ' '
-        "{0,-6} {1,-8} {2} {3}" -f $stockRecipe[0], $stockRecipe[1], $_.Value.ToString("MM/dd/yyyy")
+        "{0,-6} {1,-8} {2}" -f $stockRecipe[0], $stockRecipe[1], $_.Value.ToString("MM/dd/yyyy")
     }
 
     $sortedSymbolsList | Out-String | Write-Host
